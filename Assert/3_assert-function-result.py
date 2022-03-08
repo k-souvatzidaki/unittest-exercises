6256c68dcfe5a5ad993be3d3efd89d6859dbbfb3
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2022-03-08 21:45:36 +0200

    Assert done

--- Assert/3_assert-function-result.py
+++ Assert/3_assert-function-result.py
@@ -5,7 +5,6 @@ assert area(4,10) == 40
 assert area(5,6) == 30
 
 def area(width, height):
-    """The function returns the area of the rectangle."""
     if not (isinstance(width, int) and isinstance(height, int)):
         raise TypeError('The width and height must be of type int.')
     if not (width > 0 and height > 0):
