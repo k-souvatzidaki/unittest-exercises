7ee38999d072d600b92705734ee3d80aa14ca262
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2022-03-11 00:55:28 +0200

    test employee class done

---
+++ 7. Class Testing/18_test-employee-class.py
@@ -0,0 +1,48 @@
+import unittest
+
+# run: python -m unittest 18_test-employee-class.py
+class Employee:
+    tax_rate = 0.17
+    bonus_rate = 0.1
+
+    def __init__(self, first_name, last_name, age, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.salary = salary
+
+    @property
+    def email(self):
+        return f'{self.first_name.lower()}.{self.last_name.lower()}@mail.com'
+
+    @property
+    def tax(self):
+        return round(self.salary * self.tax_rate, 2)
+
+    def apply_bonus(self):
+        self.salary = int(self.salary * (1 + self.bonus_rate))
+
+
+class TestEmployee(unittest.TestCase):
+
+    @classmethod
+    def setUp(this):
+        this.emp = Employee('John','Smith',40,80000)
+
+    def test_email(self):
+        self.assertEqual(self.emp.email, 'john.smith@mail.com')
+
+    def test_email_after_changing_first_name(self):
+        self.emp.first_name = 'Mike'
+        self.assertEqual(self.emp.email, 'mike.smith@mail.com')
+
+    def test_email_after_changing_last_name(self):
+        self.emp.last_name = 'Doe'
+        self.assertEqual(self.emp.email, 'john.doe@mail.com')
+
+    def test_get_salary(self):
+        self.assertEqual(self.emp.salary,80000)
+
+    def test_apply_bonus(self):
+        self.emp.apply_bonus()
+        self.assertEqual(self.emp.salary,88000)
