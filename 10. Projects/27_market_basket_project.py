a32a6916a0538a13a2bae1f8292d4be9a14b5b02
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2022-03-12 16:13:58 +0200

    market basket project done

--- 10. Projects/27_market_basket_project.py
+++ 10. Projects/27_market_basket_project.py
@@ -1,5 +1,6 @@
 import unittest
 from product import Product
+from basket import ShoppingBasket
 
 class TestProduct(unittest.TestCase):
     
@@ -17,4 +18,64 @@ class TestProduct(unittest.TestCase):
         self.assertEqual(self.product.quantity, 1)
 
     def test_repr_method(self):
-        self.assertEqual(repr(self.product), "Product(name='milk', price=3.0, quantity=1)")
+        (self.assertEqual(repr(self.product), "Product(name='milk', price=3.0, quantity=1)"))
+
+
+class TestBasketWithNoProducts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(this):
+        this.basket =  ShoppingBasket()
+
+    def test_size_of_basket_should_be_empty(self):
+        self.assertEqual(len(self.basket),0)
+
+    def test_getting_product_out_of_range_should_raise_error(self):
+        self.assertRaises(IndexError,self.basket.get_product,0) # !!
+
+    def test_total_amount_should_be_zero(self):
+        self.assertEqual(self.basket.total(),0)
+
+
+class TestBasketWithOneProduct(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(this):
+        this.basket =  ShoppingBasket()
+        this.basket.add_product(Product('milk',3.0))
+
+    def test_size_of_basket_should_be_one(self):
+        self.assertEqual(len(self.basket),1)
+
+    def test_getting_product_out_of_range_should_raise_error(self):
+        self.assertRaises(IndexError,self.basket.get_product,1)
+
+    def test_total_amount_should_have_tax(self):
+        self.assertEqual(self.basket.total(),3.63)
+
+    def test_getting_product(self):
+        self.assertEqual(self.basket.get_product(0).name, 'milk')
+
+
+class TestBasketWithTwoProducts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(this):
+        this.basket =  ShoppingBasket()
+        this.basket.add_product(Product('milk',3.0))
+        this.basket.add_product(Product('water',2.0))
+
+    def test_size_of_basket_should_be_two(self):
+        self.assertEqual(len(self.basket),2)
+
+    def test_order_of_products(self):
+        self.assertEqual(self.basket.get_product(0).name, 'milk')
+        self.assertEqual(self.basket.get_product(0).price, 3.0)
+        self.assertEqual(self.basket.get_product(1).name, 'water')
+        self.assertEqual(self.basket.get_product(1).price, 2.0)
+
+    def test_total_amount_should_have_tax(self):
+        self.assertEqual(self.basket.total(),6.05)
+
+    def test_getting_product_out_of_range_should_raise_error(self):
+        self.assertRaises(IndexError,self.basket.get_product,2)
